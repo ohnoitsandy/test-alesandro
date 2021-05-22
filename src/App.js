ec168cf49b6a8f9b324bcf7377f0bf795324ea43
AlessandroAndradeRamos <aandrade8@ucol.mx>
2021-05-22 14:06:31 -0500

    Project cleaned.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-function App() {
+function App(props) {
+  const { match } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-main">
+      <Switch>
+        <Route>
+          {/*<Route path={`${match.url}/day-trip`} component={DayTrip} />*/}
+        </Route>
+      </Switch>
     </div>
   );
 }
